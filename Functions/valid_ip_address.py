7bcd5e695cbb463d253c1c9c80df9656ec950791
Russell Dreifus <131672744+br0k3ncode@users.noreply.github.com>
2024-11-04 16:32:20 +0000

    Correction to def is_valid_ipv4

--- Functions/valid_ip_address.py
+++ Functions/valid_ip_address.py
@@ -6,17 +6,17 @@ def is_valid_ipv4(ip_address):
     # Splits ip address into octets at "."
     octets = ip_address.split('.')
     # Checks there are 4 octets
-    if len(octets) != 4:
+    if len(octets) == 4:
     # Iterates through the octets
-    for octet in octets:
-        # Check that each octet can be converted to an integer and
-        # that is inclusivley between 0 and 255
-        if (octet.isdigit() and 0 <= int(octet) <= 255):
-            # If each octet is in parameters, script will continue.
-            continue
-    # If the ip_address does not meet all conditions above, the function returns False
-        else:
-            return False
+        for octet in octets:
+            # Check that each octet can be converted to an integer and
+            # that is inclusivley between 0 and 255
+            if (octet.isdigit() and 0 <= int(octet) <= 255):
+                # If each octet is in parameters, script will continue.
+                continue
+        # If the ip_address does not meet all conditions above, the function returns False
+            else:
+                return False
     # If all conditions above are met, return True.
     return True
 
